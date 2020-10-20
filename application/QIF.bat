2c8584e42f62fa11d5176ce11d63f98482bfd1c0
Michael Videlgauz <michael@videlgauz.com>
2020-10-20 22:20:20 +0300

    Start menu item is created if TargetDir is NOT under "KAYA Instruments"
    As son as  <TargetDir>@ApplicationsDir@/KAYA Instruments/SDIControlPoint</TargetDir> was changed <TargetDir>@ApplicationsDir@/SDIControlPoint</TargetDir> the item was added to start menu (despite addOperation("CreateShortcut") still failing

--- application/QIF.bat
+++ application/QIF.bat
@@ -1,12 +1,14 @@
 PATH=%PATH%;C:\Qt\5.12.9\msvc2017\bin
 SET TARGETDIR=..\..\..\__DIST\SDIControlPoint\QIF
 
-goto call_binarycreator
-
-xcopy ..\..\application\QIF %TARGETDIR%\ /e
+goto fill_qif
+::goto call_binarycreator
 
 windeployqt --verbose 2 --dir %TARGETDIR%\packages\Application\data SDIControlPoint.exe
 copy /b .\SDIControlPoint.exe %TARGETDIR%\packages\Application\data
+
+:fill_qif
+xcopy ..\..\application\QIF %TARGETDIR%\ /e /y
 
 :call_binarycreator
 pushd %TARGETDIR%
